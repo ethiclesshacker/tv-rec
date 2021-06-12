7e62d570ee910ed103e2694e911077fa778a12e0
Aditya <mailingadityavs@gmail.com>
2021-06-12 13:01:19 +0530

    Drop rows without genres.

--- genres.py
+++ genres.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 
 data = pd.read_table("tvs-data.tsv")
+data = data[data["genres"]!="\\N"]
+data.reset_index()
 
 show_genres = list(data["genres"])
 
 genres = ",".join(show_genres)
-genres = genres.replace("\\N,","")
 genres = genres.split(",")
 genres = set(genres)
 
